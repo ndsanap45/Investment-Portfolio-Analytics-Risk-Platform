1ed6a0c307aa8e00a67d46ed89690d48bcff78e5
Nagesh Sanap <ndsanap45@gmail.com>
2026-09-05 23:42:42 +0530

    Replaced seeded price data with live market data using yfinance

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,12 +1,52 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "postgresql://portfolio_user:portfolio_password@localhost:5432/portfolio_db"
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     pool_pre_ping=True
+# )
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://portfolio_user:portfolio_password@localhost:5432/portfolio_db"
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError(
+        "DATABASE_URL is not configured"
+    )
+
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -14,11 +54,13 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+
 Base = declarative_base()
 
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
